4275879fc6755844a46a2cc4a7707ac512dac03e
Edd Yerburgh <edward.yerburgh@gmail.com>
2016-12-06 17:56:55 +0000

    Test modal message is rendered

--- test/unit/containers/ModalContainer.test.jsx
+++ test/unit/containers/ModalContainer.test.jsx
@@ -1,10 +1,35 @@
 import { shallow } from 'enzyme';
 import React from 'react';
 import ModalContainer from '../../../src/containers/ModalContainer';
+import proxyquire from 'proxyquire';
+
+proxyquire.noCallThru();
 
 describe('<ModalContainer />', () => {
+  let ModalContainer;
+  let modalMessageStub;
+
+  beforeEach(() => {
+    modalMessageStub = () => <div />;
+
+    ModalContainer = proxyquire('../../../src/containers/ModalContainer', {
+      '../components/modal/ModalMessage': modalMessageStub
+        }
+    ).default
+  });
+
   it('is rendered', () => {
     const wrapper = shallow(<ModalContainer />);
     expect(wrapper).to.exist;
   });
+
+  it('renders <ModalMessage />', () => {
+    const wrapper = shallow(<ModalContainer />);
+    expect(wrapper.find(modalMessageStub)).to.have.length(1);
+  });
+  //
+  // it('passes state.modal to <ModalMessage />', () => {
+  //   const wrapper = shallow(<ModalContainer />);
+  //   expect(wrapper).to.contain('ModalMessage');
+  // });
 });
