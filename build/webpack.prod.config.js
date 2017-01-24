21428facba62a02c7d55e681bcd5973bf198ee25
Edd Yerburgh <edward.yerburgh@gmail.com>
2017-01-24 13:49:16 +0000

    Add webpack production build

---
+++ build/webpack.prod.config.js
@@ -0,0 +1,58 @@
+const { resolve } = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const config = require('config');
+const PORT = config.get('server.port');
+
+module.exports = {
+  entry: '../src/index.jsx',
+
+  output: {
+    filename: 'bundle.js',
+    path: resolve(__dirname, '../dist'),
+    publicPath: '/',
+  },
+
+  context: resolve(__dirname, '../src'),
+
+  resolve: {
+    extensions: ['.', '.js', '.jsx']
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: [
+          'babel-loader',
+        ],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.json?$/,
+        loader: 'json-loader'
+      },
+      {
+        test: /\.scss$/,
+        loaders:  ExtractTextPlugin.extract({
+          fallbackLoader: 'style-loader',
+          loader: 'css-loader!sass-loader'
+        })
+      },
+    ],
+  },
+
+  plugins: [
+    new ExtractTextPlugin('style.css'),
+    new webpack.optimize.UglifyJsPlugin(),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new HtmlWebpackPlugin({
+      template:  resolve(__dirname, '../src/index.tpl.html'),
+      inject: 'body',
+      filename: 'index.html',
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
+  ],
+};
