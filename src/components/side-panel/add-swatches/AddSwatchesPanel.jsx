ab7c5859e5e131ea18d898064320c382372a6c95
Edd Yerburgh <edward.yerburgh@gmail.com>
2017-05-24 12:17:39 +0100

    [Refactor] Move palette mapping inside return

--- src/components/side-panel/add-swatches/AddSwatchesPanel.jsx
+++ src/components/side-panel/add-swatches/AddSwatchesPanel.jsx
@@ -11,29 +11,26 @@ type Props = {
   deleteSwatches: Function
 }
 
-const AddSwatchesPanel = (props: Props): React$Element<any> => {
-  const palettes = props.palettes.map((palette, i) =>
-    <Palette
-      {...palette}
-      addNewSwatch={props.addNewSwatch}
-      key={`palette${i}`}
-    />);
-  return (
-    <div>
-      <p>Enter a valid RGB, hex or CSS to add a swatch</p>
-      <AddSwatchForm addNewSwatch={props.addNewSwatch} />
-      <p>Or choose from some pre made palettes</p>
-      {palettes}
-      <hr />
-      <button
-        className="btn"
-        id="delete-all"
-        onClick={props.deleteSwatches}
-      >
+const AddSwatchesPanel = (props: Props): React$Element<any> => (
+  <div>
+    <p>Enter a valid RGB, hex or CSS to add a swatch</p>
+    <AddSwatchForm addNewSwatch={props.addNewSwatch} />
+    <p>Or choose from some pre made palettes</p>
+    {props.palettes.map((palette, i) =>
+      <Palette
+        {...palette}
+        addNewSwatch={props.addNewSwatch}
+        key={`palette${i}`}
+      />)}
+    <hr />
+    <button
+      className="btn"
+      id="delete-all"
+      onClick={props.deleteSwatches}
+    >
           Delete all
       </button>
-    </div>
-  );
-};
+  </div>
+);
 
 export default AddSwatchesPanel;
