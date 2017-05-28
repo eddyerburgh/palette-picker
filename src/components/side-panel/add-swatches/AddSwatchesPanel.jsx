7c2148b5acec7145079fa907eed88a555e95700a
Edd Yerburgh <edward.yerburgh@gmail.com>
2017-05-28 20:09:22 +0100

    [Style] Add titles to swatch panel

--- src/components/side-panel/add-swatches/AddSwatchesPanel.jsx
+++ src/components/side-panel/add-swatches/AddSwatchesPanel.jsx
@@ -13,13 +13,16 @@ type Props = {
 
 const AddSwatchesPanel = (props: Props): React$Element<any> => (
   <div>
+    <h4>Custom swatch</h4>
     <AddSwatchForm addNewSwatch={props.addNewSwatch} />
+    <h4 className="mt-10">Pre-made palettes</h4>
     {props.palettes.map((palette, i) =>
       <Palette
         {...palette}
         addNewSwatch={props.addNewSwatch}
         key={`palette${i}`}
       />)}
+    <h4 className="mt-10">Options</h4>
     <button
       className="btn btn-primary btn-block"
       id="delete-all"
